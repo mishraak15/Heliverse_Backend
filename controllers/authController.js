7e3f96c064f4b110c4a6886472ad015fbf73b036
mishraak15 <133697079+mishraak15@users.noreply.github.com>
2024-08-13 12:47:49 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,13 +12,13 @@ const signToken = (id) => {
 };
 
 const createSendToken = (id, statusCode, res) => {
- const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+const token = signToken(id);
+  const cookieExpiryDays = parseInt(process.env.JWT_COOKIE_EXPIRES_IN, 10);
+  const cookieOptions = {
+    expires: new Date(Date.now() + cookieExpiryDays * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process?.env?.NODE_ENV === "production",
-    sameSite: 'Lax'
+    sameSite: 'Lax',
   };
 
   res.cookie("jwt", token, cookieOptions);
